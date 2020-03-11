4e3d538557ee1e86297799f1ead3d22a29c6c5b9
dexslab <dex35803@gmail.com>
2020-03-11 07:06:38 -0400

    Added FQBN to allow both board builds

--- Models/Board.cs
+++ Models/Board.cs
@@ -10,12 +10,15 @@ namespace BaseStationInstaller.Models
 
     public class Board
     {
-        public Board(string name, ArduinoModel platform)
+        public Board(string name, ArduinoModel platform, string fqbn)
         {
             Name = name;
             Platform = platform;
+            FQBN = fqbn;
         }
         public string Name { get; set; }
         public ArduinoModel Platform { get; set; }
+
+        public string FQBN;
     }
 }
