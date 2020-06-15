848522cb4458187dfbcc00e87a13d5693859bfad
Dex's Lab <dex35803@gmail.com>
2020-06-15 12:11:58 -0400

    Fixes to git merges

---
+++ BaseStationInstaller/ViewModels/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace BaseStationInstaller.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+        
+    }
+}
---
+++ BaseStationInstaller/Views/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BaseStationInstaller.Views
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
