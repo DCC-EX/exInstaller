e78281a3f37a36a404f085d638ed14b6aba142bb
dexslab <dex35803@gmail.com>
2021-02-26 23:57:38 -0500

    fixes for publish

--- publish.ps1
+++ publish.ps1
@@ -5,10 +5,35 @@ dotnet publish -c release -r osx-x64
 dotnet publish -c release -r linux-arm64
 dotnet publish -c release -r linux-arm
 del exInstaller-*
-wsl tar -czf exInstaller-arm.tar.gz "BaseStationInstaller/bin/Release/netcoreapp3.1/linux-arm/publish/*"
-wsl tar -czf exInstaller-arm64.tar.gz "BaseStationInstaller/bin/Release/netcoreapp3.1/linux-arm64/publish/*"
-wsl tar -czf exInstaller-linux-x64.tar.gz "BaseStationInstaller/bin/Release/netcoreapp3.1/linux-x64/publish/*"
-wsl zip -r exInstaller-win-x86.zip "BaseStationInstaller/bin/Release/netcoreapp3.1/win-x86/publish"
-wsl zip -r exInstaller-win-x64.zip "BaseStationInstaller/bin/Release/netcoreapp3.1/win-x64/publish"
-wsl tar -czf exInstaller-osx.tar.gz "BaseStationInstaller/bin/Release/netcoreapp3.1/osx-x64/publish/*"
+cd "F:\TrainStuff\exInstaller\BaseStationInstaller\bin\Release\netcoreapp3.1\linux-arm\publish"
+Remove-Item arduino-cli-runtimes\*_32bit -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes\*_*64* -Recurse -Force -Confirm:$false
+wsl tar -vczf /mnt/f/TrainStuff/exInstaller/exInstaller-linux-arm.tar.gz *
+cd "F:\TrainStuff\exInstaller\BaseStationInstaller\bin\Release\netcoreapp3.1\linux-arm64\publish"
+Remove-Item arduino-cli-runtimes\*_32bit -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes\*_64bit -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes\*_ARM -Recurse -Force -Confirm:$false
+wsl  tar -vczf /mnt/f/TrainStuff/exInstaller/exInstaller-linux-arm64.tar.gz *
+cd "F:\TrainStuff\exInstaller\BaseStationInstaller\bin\Release\netcoreapp3.1\linux-x64\publish"
+Remove-Item arduino-cli-runtimes\Win* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes\*ARM* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes\mac* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes\Linux_32bit -Recurse -Force -Confirm:$false
+wsl  tar -vczf /mnt/f/TrainStuff/exInstaller/exInstaller-linux-x64.tar.gz *
+cd "F:\TrainStuff\exInstaller\BaseStationInstaller\bin\Release\netcoreapp3.1\win-x86\publish"
+Remove-Item arduino-cli-runtimes\Linux* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes\mac* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes\*_64bit -Recurse -Force -Confirm:$false
+wsl  zip -r /mnt/f/TrainStuff/exInstaller/exInstaller-win-x86.zip *
+cd "F:\TrainStuff\exInstaller\BaseStationInstaller\bin\Release\netcoreapp3.1\win-x64\publish"
+Remove-Item arduino-cli-runtimes\Linux* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes\mac* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes\*_32bit -Recurse -Force -Confirm:$false
+wsl  zip -r /mnt/f/TrainStuff/exInstaller/exInstaller-win-x64.zip *
+cd "F:\TrainStuff\exInstaller\BaseStationInstaller\bin\Release\netcoreapp3.1\osx-x64\publish"
+Remove-Item arduino-cli-runtimes\Linux* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes\Win* -Recurse -Force -Confirm:$false
+wsl tar -vczf /mnt/f/TrainStuff/exInstaller/exInstaller-osx-x64.tar.gz *
+cd F:\TrainStuff\exInstaller
+
 
