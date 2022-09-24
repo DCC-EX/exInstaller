7b12b0bd4168bb03cab92493f3cf7db19790b11e
Dex's Lab <dex35803@gmail.com>
2022-09-24 16:18:13 -0400

    updates for publish, update version

--- publish.ps1
+++ publish.ps1
@@ -5,45 +5,46 @@ dotnet publish -c release -r osx-x64
 dotnet publish -c release -r linux-arm64
 dotnet publish -c release -r linux-arm
 del exInstaller-*
-cd "exInstaller\bin\Release\netcoreapp5.0\linux-arm\publish"
-Remove-Item arduino-cli-runtimes\*_32bit -Recurse -Force -Confirm:$false
-Remove-Item arduino-cli-runtimes\*_*64* -Recurse -Force -Confirm:$false
-7z a  -ttar ..\..\..\..\..\..\exInstaller-linux-arm.tar
-cd ..\..\..\..\..\..
-echo 7z a .\exInstaller.tar -tgzip .\exInstaller-linux-arm.tar.gz
-echo del exInstaller.tar
-cd "exInstaller\bin\Release\netcoreapp5.0\linux-arm64\publish"
-Remove-Item arduino-cli-runtimes\*_32bit -Recurse -Force -Confirm:$false
-Remove-Item arduino-cli-runtimes\*_64bit -Recurse -Force -Confirm:$false
-Remove-Item arduino-cli-runtimes\*_ARM -Recurse -Force -Confirm:$false
-7z a  -ttar ..\..\..\..\..\..\exInstaller-linux-arm64.tar
-cd ..\..\..\..\..\..
-echo 7z a .\exInstaller.tar -tgzip .\exInstaller-linux-arm64.tar.gz
-echo del exInstaller.tar
-cd "exInstaller\bin\Release\netcoreapp5.0\linux-x64\publish"
-Remove-Item arduino-cli-runtimes\Win* -Recurse -Force -Confirm:$false
-Remove-Item arduino-cli-runtimes\*ARM* -Recurse -Force -Confirm:$false
-Remove-Item arduino-cli-runtimes\mac* -Recurse -Force -Confirm:$false
-Remove-Item arduino-cli-runtimes\Linux_32bit -Recurse -Force -Confirm:$false
-7z a  -ttar ..\..\..\..\..\..\exInstaller-linux-x64.tar
-cd ..\..\..\..\..\..
-echo 7z a .\exInstaller.tar -tgzip .\exInstaller-linux-x64.tar.gz
-echo del exInstaller.tar
-cd "exInstaller\bin\Release\netcoreapp5.0\win-x86\publish"
-Remove-Item arduino-cli-runtimes\Linux* -Recurse -Force -Confirm:$false
-Remove-Item arduino-cli-runtimes\mac* -Recurse -Force -Confirm:$false
-Remove-Item arduino-cli-runtimes\*_64bit -Recurse -Force -Confirm:$false
-7z a  -tzip ..\..\..\..\..\..\exInstaller-win-x86.zip
-cd ..\..\..\..\..\..
-cd "exInstaller\bin\Release\netcoreapp5.0\win-x64\publish"
-Remove-Item arduino-cli-runtimes\Linux* -Recurse -Force -Confirm:$false
-Remove-Item arduino-cli-runtimes\mac* -Recurse -Force -Confirm:$false
-Remove-Item arduino-cli-runtimes\*_32bit -Recurse -Force -Confirm:$false
-7z a  -tzip ..\..\..\..\..\..\exInstaller-win-x64.zip
-cd ..\..\..\..\..\..
-cd "exInstaller\bin\Release\netcoreapp5.0\osx-x64\publish"
-Remove-Item arduino-cli-runtimes\Linux* -Recurse -Force -Confirm:$false
-Remove-Item arduino-cli-runtimes\Win* -Recurse -Force -Confirm:$false
-7z a  -ttar ..\..\..\..\..\..\exInstaller-osx-x64.tar
-cd ..\..\..\..\..\..
-
+cd "exInstaller/bin/Release/netcoreapp6.0/linux-arm/publish"
+Remove-Item arduino-cli-runtimes/*_32bit -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes/*_*64* -Recurse -Force -Confirm:$false
+7z a -ttar "../../../../../../exInstaller-linux-arm.tar"
+cd "../../../../../.."
+echo 7z a ./exInstaller-linux-arm.tar -tgzip ./exInstaller-linux-arm.tar.gz
+echo del exInstaller-linux-arm.tar
+cd "exInstaller/bin/Release/netcoreapp6.0/linux-arm64/publish"
+Remove-Item arduino-cli-runtimes/*_32bit -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes/*_64bit -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes/*_ARM -Recurse -Force -Confirm:$false
+7z a -ttar "../../../../../../exInstaller-linux-arm64.tar"
+cd "../../../../../.."
+echo 7z a ./exInstaller-linux-arm64.tar -tgzip ./exInstaller-linux-arm64.tar.gz
+echo del exInstaller-linux-arm64.tar
+cd "exInstaller/bin/Release/netcoreapp6.0/linux-x64/publish"
+Remove-Item arduino-cli-runtimes/Win* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes/*ARM* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes/mac* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes/Linux_32bit -Recurse -Force -Confirm:$false
+7z a -ttar "../../../../../../exInstaller-linux-x64.tar"
+cd "../../../../../.."
+echo 7z a ./exInstaller-linux-x64.tar -tgzip ./exInstaller-linux-x64.tar.gz
+echo del exInstaller-linux-x64.tar
+cd "exInstaller/bin/Release/netcoreapp6.0/win-x86/publish"
+Remove-Item arduino-cli-runtimes/Linux* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes/mac* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes/*_64bit -Recurse -Force -Confirm:$false
+7z a -tzip "../../../../../../exInstaller-win-x86.zip"
+cd "../../../../../.."
+cd "exInstaller/bin/Release/netcoreapp6.0/win-x64/publish"
+Remove-Item arduino-cli-runtimes/Linux* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes/mac* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes/*_32bit -Recurse -Force -Confirm:$false
+7z a -tzip "../../../../../../exInstaller-win-x64.zip"
+cd "../../../../../.."
+cd "exInstaller/bin/Release/netcoreapp6.0/osx-x64/publish"
+Remove-Item arduino-cli-runtimes/Linux* -Recurse -Force -Confirm:$false
+Remove-Item arduino-cli-runtimes/Win* -Recurse -Force -Confirm:$false
+7z a -ttar "../../../../../../exInstaller-osx-x64.tar"
+cd "../../../../../.."
+echo 7z a ./exInstaller-osx-x64.tar -tgzip ./exInstaller-osx-x64.tar.gz
+echo del exInstaller-osx-x64.tar
